383a8e15a02bca0ef3394cd1f6a3a35231749f7a
Syedmoiz7 <syedzada7866@gmail.com>
2022-10-17 12:20:41 +0500

    initial commit

---
+++ src/components/index.jsx
@@ -0,0 +1,10 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios' ;
+import './index.css';
+
+let NewsFeed = () => {
+    const [news, setNews] = useState([])
+
+}
+
+export default NewsFeed;
